3cd8ff26dcdcc72ccea74b2393963829d0d23450
Hustler-1 <calvin.whitney@selu.edu>
2024-10-01 19:10:10 +0000

    first draft of math function

---
+++ R/meanr.R
@@ -0,0 +1,10 @@
+meanr = function(path, column){
+  result =  dataframe %>%
+    filter(!is.na({{column}})) %>%
+    pull({{column}}) %>%
+    mean()
+  if(sum(is.na(result)) == 0){
+    return(result)
+  } else
+    print("There are still remaining NA values. Either the function failed to catch some NA values, the data has some missing data not deliminated by NA, or the column deliminated contains nonnumeric data. Clean the data and try again.")
+}
